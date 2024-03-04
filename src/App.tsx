e3811a006c8df80f5b3974b1a4465deeedc94830
Dantiante <davidlaupsa@outlook.com>
2024-03-04 17:25:04 +0100

    Verified with Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,59 @@
 import './App.css';
-import "ol/ol.css";
-import {Map, View} from "ol/"
-import { MutableRefObject, useEffect, useRef, useState } from 'react';
-import TileLayer from 'ol/layer/Tile';
-import {OSM} from "ol/source"
-import { useGeographic } from 'ol/proj';
-import { useCivilDefenseLayerCheckbox } from './Modules/civilDefense';
-import { Layer } from 'ol/layer';
-import emergencyShelterCheckbox from './Modules/emergencyShelter';
+import { MutableRefObject, useEffect, useRef, useState } from "react";
+import { Map, View } from "ol/";
+import { Layer } from "ol/layer";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+import { useEmergencyShelterCheckbox } from "./Modules/emergencyShelter";
+import { useCivilDefenseLayerCheckbox } from "./Modules/civilDefense";
 
-
-
-// I have tried making it work so that it start on Oslo, but I get an error when I use run npm lint, it tells me to put
-// useGeographic() at the top of the function, and I have no idea why if I put the view in there, then the checkboxes do not work
-// So I decided to just let it stay at middle of the world
-const view = new View ({
+const view = new View({
   center: [0, 0],
-  zoom:8
-})
+  zoom: 8,
+});
 
 const map = new Map({
   layers: [
     new TileLayer({
-      source: new OSM()
-    })
+      source: new OSM(),
+    }),
   ],
-  view: view
+  view: view,
 });
 
-
 export function App() {
-
   useGeographic();
 
-
-
-
   const [layers, setLayers] = useState<Layer[]>([
-    new TileLayer({ source: new OSM}),
+    new TileLayer({ source: new OSM() }),
   ]);
-  
 
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
-  useEffect(() => {map.setTarget(mapRef.current);}, []);
-  useEffect(() => map.setLayers(layers), [layers]);
 
-  
-  
-  
+  useEffect(() => {
+    map.setTarget(mapRef.current);
+  }, []);
+
+  useEffect(() => {
+    map.setLayers(layers);
+  }, [layers]);
+
   return (
     <>
+      <header>
+        <h1 className="header">Map application</h1>
+      </header>
 
+      <nav className="navBar">
+        Actions:
+        {useCivilDefenseLayerCheckbox({ map, setLayers, layers })}
+        {useEmergencyShelterCheckbox({ map, setLayers, layers })}
+      </nav>
 
-    <header><h1 className='header'>Map application</h1></header>
-    
-    <nav className='navBar'>Actions:
-        {useCivilDefenseLayerCheckbox({ map, setLayers, layers})}
-        {emergencyShelterCheckbox({map, setLayers, layers})}
-    </nav>
-    <main className='map' ref={mapRef}></main>
- 
-
-  </>
+      <main className="map" ref={mapRef}></main>
+    </>
   );
-
 }
 
-export default App
+export default App;
