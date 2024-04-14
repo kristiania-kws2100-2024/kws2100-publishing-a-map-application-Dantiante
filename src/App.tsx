ca6377362d3903c9cbde7796d7af04be1f971a96
Dantiante <160004811+Dantiante@users.noreply.github.com>
2024-04-14 17:04:36 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { OSM } from "ol/source";
 import { useGeographic } from "ol/proj";
 import { useEmergencyShelterCheckbox } from "./Modules/emergencyShelter";
 import { useCivilDefenseLayerCheckbox } from "./Modules/civilDefense";
+import React from "react";
 
 // For some reason, no matter how I put it, it was either, start in the center of the map,
 // or the handleClicks/hover for the checkbox' did not work, so I chose to put it in the center of the world
